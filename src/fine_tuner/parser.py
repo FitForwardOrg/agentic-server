64e4a297cb218576f56b719387409dee7347d2e2
shuvava <shuvava@users.noreply.github.com>
2026-02-06 15:36:37 -0800

    fix lint errors

--- src/fine_tuner/parser.py
+++ src/fine_tuner/parser.py
@@ -2,16 +2,18 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def mk_doc_converter(source: str) -> str:
     """Convert document of supported formats to Markdown.
     It returns an empty string if the conversion fails.
     """
     # model loading takes ~3sec on a dev laptop, so we do lazy loading
     from docling.document_converter import DocumentConverter
+
     converter = DocumentConverter()
     try:
         result = converter.convert(source)
         return result.document.export_to_markdown()
     except Exception as e:
-        logger.error(f"Error converting document:", exc_info=e)
+        logger.error("Error converting document:", exc_info=e)
         return ""
